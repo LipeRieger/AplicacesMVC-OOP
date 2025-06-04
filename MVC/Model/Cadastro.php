14a228385f614c5168b18ea78e2fa88433de3328
Felipe Rieger <liperieger@gmail.com>
2025-06-04 08:36:07 -0300

    Atualização

---
+++ MVC/Model/Cadastro.php
@@ -0,0 +1,11 @@
+<?php
+$acao = $_GET['acao']
+
+$usuarios = [];
+
+if($acao = 'salvar'){
+    $nome = $_POST['nome']
+    $email = $_POST['email']
+}
+
+?>
---
+++ MVC/View/View.php
@@ -0,0 +1,7 @@
+
+    <h2>Cadastrar Usuário</h2>
+    <form method="POST">
+        <input type="text" name="nome"><br>
+        <input type="text" name="email"><br>
+        <button type="submit">Salvar</button>
+    </form>
